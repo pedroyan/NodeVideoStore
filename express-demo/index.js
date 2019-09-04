a318087b6c59bc168c6f0e61e50c228427039dfb
Pedro Yan Ornelas <pedroyane@gmail.com>
2019-09-04 17:07:12 -0300

    Courses lookup

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name: 'coursename'},
+    {id: 2, name: 'coursename 2'},
+    {id: 3, name: 'coursename 3'},
+    {id: 4, name: 'coursename 4'},
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3,4,5]);
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const desiredCourse = courses.find(c => c.id === parseInt(req.params.id))
+
+    if(!desiredCourse)
+        res.status(404).send('The course with the given ID could not be found');
+
+    res.send(desiredCourse);
 });
 
 app.get('/api/courses/:year/:month', (req, res) => {
