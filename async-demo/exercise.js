e030cf629f674334976cd6759eb75652bd87954d
Pedro Yan Ornelas <pedroyane@gmail.com>
2019-09-05 17:11:16 -0300

    Finished exercise

--- async-demo/exercise.js
+++ async-demo/exercise.js
@@ -1,35 +1,46 @@
+(async function(){
+    try{
+        const customer = await getCustomer(2);
+        console.log('Customer: ', customer);
+        
+        if (customer.isGold) {
+            const movies = await getTopMovies();
+            console.log('Top movies: ', movies);
+            await sendEmail();
+            console.log('Email sent...');
+        }
+    }catch(err){
+        console.log(err);
+    }
+})()
 
-getCustomer(1, (customer) => {
-  console.log('Customer: ', customer);
-  if (customer.isGold) {
-    getTopMovies((movies) => {
-      console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
-      });
-    });
-  }
-});
+function getCustomer(id) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                id: id,
+                name: 'Mosh Hamedani',
+                isGold: true,
+                email: 'email'
+            });
+        }, 4000);
+    })
 
-function getCustomer(id, callback) {
-  setTimeout(() => {
-    callback({ 
-      id: 1, 
-      name: 'Mosh Hamedani', 
-      isGold: true, 
-      email: 'email' 
-    });
-  }, 4000);  
 }
 
-function getTopMovies(callback) {
-  setTimeout(() => {
-    callback(['movie1', 'movie2']);
-  }, 4000);
+function getTopMovies() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(['movie1', 'movie2']);
+        }, 4000);
+    });
+
 }
 
 function sendEmail(email, movies, callback) {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, 4000);
+    });
 }
