1ca90aa2c97015588577ec4ca211a174c6fb6301
Pedro Yan Ornelas <pedroyane@gmail.com>
2019-09-05 16:52:46 -0300

    Async-Await block

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,28 @@
 console.log('Before');
 
-getUser(2)
-    .then(u => getRepositories(u.gitHubUserName))
-    .then(r => getCommits(r[0]))
-    .then(c => console.log('Commits fetched', c))
-    .catch(err => console.error('An error occurred!', err));
+//Promise-based approach
+// getUser(2)
+//     .then(u => getRepositories(u.gitHubUserName))
+//     .then(r => getCommits(r[0]))
+//     .then(c => console.log('Commits fetched', c))
+//     .catch(err => console.error('An error occurred!', err));
 
+//Async await method
+async function displayCommits(){
+
+    try{
+        const user = await getUser(2);
+        const repos = await getRepositories(user.gitHubUserName);
+        const commits = await getCommits(repos[0]);
+    
+        console.log('Commits fetched', commits);
+    } catch (err){
+        console.log('Error', err);
+    }
+
+}
+
+displayCommits();
 console.log('After');
 
 function getUser(id){
@@ -21,6 +38,7 @@ function getRepositories(username){
     return new Promise((resolve, reject) => {
         console.log(`Fetching repos for user ${username}`);
         setTimeout(() => {
+            //reject(new Error('Unauthorized user'));
             resolve(['repo1', 'repo2', 'repo3']);
         }, 2000);
     });
