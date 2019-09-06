155bd58e8631f733c787e8c5630c4a56f90ea09d
Pedro Yan Ornelas <pedroyane@gmail.com>
2019-09-06 16:37:19 -0300

    Finished playground

---
+++ mongo-demo/index.js
@@ -0,0 +1,108 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to MongoDb...'))
+    .catch(err => console.log('Could not connect to MongoDB', err));
+
+//Defines the course schema
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+    price: Number,
+});
+
+//Creates the Course class
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    //Creates the course instance
+    const nodeCourse = new Course({
+        name: 'C# Course',
+        author: 'Pedro',
+        tags: ['.NET', 'Backend'],
+        isPublished: false,
+        price: 50
+    });
+
+    const result = await nodeCourse.save();
+    console.log('Saved new course', result._id);
+}
+
+//createCourse();
+//getCourses();
+//getFilteredCourses();
+getPaginatedCourses()
+
+async function getCourses() {
+    const courses = await Course.find();
+    console.log('Listing all courses: ', courses);
+}
+
+async function getFilteredCourses() {
+    //Mongo db comparison operators:
+    //eq (equal)
+    //ne (not equal)
+    //gt (greater than)
+    //gte (greater than or equal to)
+    //lt (less than)
+    //lte (less than or equal to)
+    //in
+    //nin (not in)
+
+    //Logical operators:
+    //or
+    //and
+
+    const queried = await Course
+        //.find({price: 15, author: 'Pedro'})               //I want a course with the price of 15 dollars made by Pedro
+        //.find({price: {$lt: 19, $gt:6}})                  //I want courses with their prices between 19 and 6 dollars
+        //.find({ price: { $in: [15, 8, 5] } })             //I want courses where their prices match any of the entered values7
+        .find()
+        .or([{ isPublished: true }, { author: 'Pedro Yan' }])
+        .and({ price: { $gt: 10 } })                            //SQL Equivalent: where (isPublished = true || author: 'Pedro') && price > 10
+        .limit(10)
+        .sort({ name: 1 }) //1 = Ascending order, -1 = Descending order
+    //.select({ name: 1, tags: 1, price: 1 }); //Properties selected in the projection returned
+    //.count(); //Get only the number of results in the query
+
+    console.log('Queried courses', queried);
+
+    //getRegexedCourses
+    //getPaginatedCourses
+}
+
+async function getRegexedCourses() {
+
+    const queried = await Course
+        //String starts with Pedro
+        .find({ author: /^Pedro/ })
+        .limit(10)
+        .sort({ name: 1 }) //1 = Ascending order, -1 = Descending order
+        .select({ name: 1, tags: 1 }); //Properties selected in the projection returned
+
+    console.log('Queried courses', queried);
+}
+
+async function getPaginatedCourses() {
+
+    const pageSize = 10;
+    const pageNumber = 2;
+
+    const queried = await Course
+        //String starts with Pedro
+        .find({ author: /^Pedro/ })
+        .skip(pageSize * (pageNumber - 1))
+        .limit(pageSize)
+        .sort({ name: 1 }) //1 = Ascending order, -1 = Descending order
+        .select({ name: 1, tags: 1 }); //Properties selected in the projection returned
+
+    console.log('Queried courses', queried);
+}
+
+
+
+
+
