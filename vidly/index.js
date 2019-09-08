02c51ba99af415ab219c9ed7807f6de3b9982bd5
Pedro Yan Ornelas <pedroyane@gmail.com>
2019-09-08 12:41:58 -0300

    Implemented simple password comparison API

--- vidly/index.js
+++ vidly/index.js
@@ -10,6 +10,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true})
     .then(() => debug('Connected to the database...'))
@@ -23,6 +24,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.NODEPORT || 7345;
 app.listen(port, () => {
---
+++ vidly/routes/auth.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const _ = require('lodash');
+const router = express.Router();
+const { User } = require('../models/user');
+const debug = require('debug')('app:auth');
+
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Invalid credentials');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid credentials');
+
+
+    res.send(true);
+});
+
+const loginJoiSchema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+};
+
+function validate(request){
+    return Joi.validate(request, loginJoiSchema);
+}
+
+module.exports = router;
