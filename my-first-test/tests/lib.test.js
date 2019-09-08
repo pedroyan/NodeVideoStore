410e65dacdbdbeab13a6b42c78368f851a1873e0
Pedro Yan Ornelas <pedroyane@gmail.com>
2019-09-08 18:40:36 -0300

    Group tests by function name

--- my-first-test/tests/lib.test.js
+++ my-first-test/tests/lib.test.js
@@ -1,17 +1,20 @@
 //docs on: https://jestjs.io/docs/en/getting-started
 const lib = require('../lib');
 
-test('absolute - should return posite if input is positive', () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-});
+describe('absolute' , () => { //Used for grouping tests
 
-test('absolute - should return posite if input is negative', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
-
-test('absolute - should return 0 if input is 0', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
+    it('should return posite if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return posite if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return 0 if input is 0', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
 });
