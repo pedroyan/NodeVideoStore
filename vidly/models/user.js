e3d89c36579e53b171258d376bfb5660cedd852c
Pedro Yan Ornelas <pedroyane@gmail.com>
2019-09-08 01:34:30 -0300

    Created the User model
    squash

---
+++ vidly/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+// Register: POST /api/user {name, email, password}. 
+//Email must be unique -> { email: {type: String, unique:true}}
+// Login: POST /api/logins
+
+const User = mongoose.model('User', new mongoose.Schema({
+    name:{
+        type: String,
+        minlength: 5,
+        maxlength: 255,
+        trim: true
+    },
+    email:{
+        type: String,
+        minlength: 5,
+        maxlength: 255,
+        unique: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        minlength: 5,
+        maxlength: 255,
+    }
+}));
+
+const userJoiSchema = {
+    name: Joi.string().min(5).max(255).required(),
+    email: Joi.string().min(5).max(255).required(), //ID to genre
+    password: Joi.string().min(5).max(255).required(),
+};
+
+function validate(user){
+    return Joi.validate(user, userJoiSchema);
+}
+
+
+exports.User = User;
+exports.validate = validate;
---
+++ vidly/routes/users.js
@@ -0,0 +1,5 @@
+// Authentication
+// Authorization
+
+// Register: POST /api/user {name, email, password}. Email must be unique -> { email: {type: String, unique:true}}
+// Login: POST /api/logins
