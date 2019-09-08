ba72f8c930ed70a0e815485b5a5f44898dc2d83e
Pedro Yan Ornelas <pedroyane@gmail.com>
2019-09-08 12:22:18 -0300

    Salted the password using the bcrypt library

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const express = require('express');
 const _ = require('lodash');
 const router = express.Router();
@@ -18,9 +19,10 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({email: req.body.email});
     if (user) return res.status(400).send('User already registered');
 
-    // I KNOW! This is a crime. We need to hash the password before saving it to the DB, but for the sake of following the course structure I will save it unhashed
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
     try{
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
         await user.save();
 
         //Builds a new model containing only the name and email property
